a90e9fd56969ab3433123a36f30efea14ec543be
Wilson <wilson@wilsondeimac.local>
2016-07-13 13:46:39 +0800

    Initial Commit

---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by Wilson on 16/7/13.
+//  Copyright © 2016年 Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by Wilson on 16/7/13.
+//  Copyright © 2016年 Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
